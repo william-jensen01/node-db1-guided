353be214751e8327b8ca9d461fa7821b2ee11406
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-01-14 18:27:15 +0100

    wip

--- data/db-config.js
+++ data/db-config.js
@@ -1,5 +1,7 @@
-const knex = require('knex');
+const knex = require('knex'); // to make sql queries using javascript
 
-const config = require('../knexfile.js');
+const config = require('../knexfile.js'); // different configs for different envs
 
-module.exports = knex(config.development);
+
+
+module.exports = knex(config.development); // choose env and call kenx with the right config
