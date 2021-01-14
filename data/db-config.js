b0ad8731fd3aaff90262f32236487e77a677fc55
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-01-14 18:34:28 +0100

    wip

--- data/db-config.js
+++ data/db-config.js
@@ -9,4 +9,4 @@ const env = process.env.NODE_ENV || 'development'
 
 console.log('the env is', env)
 
-module.exports = knex(config[env]); // choose env and call kenx with the right config
+module.exports = knex(config[env]); // this code will work for both envs
