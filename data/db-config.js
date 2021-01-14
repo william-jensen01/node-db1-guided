df1634f2b52230dea532461aeba253d8f03e39ed
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-01-14 18:28:52 +0100

    wip

--- data/db-config.js
+++ data/db-config.js
@@ -3,6 +3,8 @@ const knex = require('knex'); // to make sql queries using javascript
 const config = require('../knexfile.js'); // different configs for different envs
 
 // in HEROKU, NODE_ENV // "production"
-const env = process.env.NODE_ENV
+const env = process.env.NODE_ENV || 'development'
+
+console.log('the env is', env)
 
 module.exports = knex(config.development); // choose env and call kenx with the right config
