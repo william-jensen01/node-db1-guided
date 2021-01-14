1b6352f0d1632b97c3cb2e94774faf126b3b9f87
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-01-14 18:19:20 +0100

    wip

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 require('dotenv').config()
 const server = require('./api/server.js')
 
-const PORT =  process.env.PORT || 5000
+const PORT = process.env.PORT || 5000
+
+console.log(process.env.FOO)
 
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`)
