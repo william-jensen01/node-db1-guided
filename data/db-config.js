18e8e0cb176584e68615d1fdb6b683942df9b5dc
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-01-14 18:33:39 +0100

    wip

--- data/db-config.js
+++ data/db-config.js
@@ -3,6 +3,8 @@ const knex = require('knex'); // to make sql queries using javascript
 const config = require('../knexfile.js'); // different configs for different envs
 
 // in HEROKU, NODE_ENV // "production"
+// in local, NODE_ENV // undefined
+// so we use a logical expression to get the environment
 const env = process.env.NODE_ENV || 'development'
 
 console.log('the env is', env)
