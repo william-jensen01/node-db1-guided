f25d6bc94d689938472339f7ea11e8d0ece332ac
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-01-14 18:23:18 +0100

    wip

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,5 @@
 module.exports = {
+  // for my machine
   development: {
     client: 'sqlite3',
     connection: {
@@ -12,8 +13,18 @@ module.exports = {
       directory: './data/seeds',
     },
   },
-
+  // for heroku (maybe postgres instead of sqlite)
   production: {
-    
+    client: 'sqlite3',
+    connection: {
+      filename: './data/posts.db3',
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
   }
 };
