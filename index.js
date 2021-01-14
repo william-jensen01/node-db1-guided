707cf251ffc429af89517fc6b4fbd1092a3bef23
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-01-14 18:20:09 +0100

    wip

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const server = require('./api/server.js')
 
 const PORT = process.env.PORT || 5000
 
-console.log(process.env.FOO)
-
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`)
 })
