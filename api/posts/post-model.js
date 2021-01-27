368776a62905ccf64fdc58a5589dec22ce016de2
William Jensen <williambjensen01@gmail.com>
2021-01-27 15:00:13 -0700

    completed guided project

--- api/posts/post-model.js
+++ api/posts/post-model.js
@@ -1,3 +1,5 @@
+const db = require('../../data/db-config');
+
 module.exports = {
   get,
   getById,
@@ -6,22 +8,67 @@ module.exports = {
   remove,
 }
 
-function get() {
-  return Promise.resolve('get wired')
+async function get() {
+  // knex will return an array of objects (the posts)
+
+  const posts = await db('posts'); // db.select('*').from('posts')
+    // select * from 'posts'
+
+  return posts;
 }
 
-function getById() {
-  return Promise.resolve('getById wired')
+async function getById(id) {
+  // id will return a number
+  // { id } will return {id: number}
+  // knex needs { id } to find the post with specified id
+
+  // knex will return an array of objects that meet the criteria given in the .where
+  // if knex finds 3 objects it will return an array of 3 objects
+  // if knex finds 0 objects it will return an array of 0 objects
+  // in the checkId function of post-router.js we are checking if the post is assigned a value
+  // an empty array is a valid value
+  // so we want to make post -> [post] because we are taking the first returned value and assigning it to post
+  // if it is an empty array, the first element is undefined and post will not have a value ... if it is an array of 3 objects the first element of the 3 will be returned as post
+
+  const [post] = await db('posts').where({ id });
+    // SELECT * FROM 'posts' WHERE 'id' = id
+
+  return post;
 }
 
-function create() {
-  return Promise.resolve('create wired')
+async function create(data) {
+  // when using insert() knex will return an array with the new post's id
+  // we know it's an array so we make postId -> [postId] (we dereference it) so instead of the value being an array of numbers it will only be the number itself
+  // doing it this way means the req.body can only accept one posts object
+
+  const [postId] = await db('posts').insert(data);
+    // INSERT INTO 'posts' ('contents', 'title') VALUES (*contents*, *title*)
+
+  // now that we have the id by itself, we'll want to look up the post using getById()
+  const post = await getById(postId)
+
+  // once we receive the specific post we'll want to return it
+  return post;
 }
 
-function update() {
-  return Promise.resolve('update wired')
+async function update(id, changes) {
+  // when using update(), knex will return a number of rows that were successfully changed
+  const count = await db('posts').where({ id }).update(changes);
+  return count;
 }
 
-function remove() {
-  return Promise.resolve('delete wired')
+// function updateThen(id, changes) {
+//   db('posts').where({ id }).update(changes)
+//     .then(count => {
+//       return count;
+//     })
+//     .catch(err => {
+//       throw err;
+//     })
+// }
+
+async function remove(id) {
+  // similar to update(), when using del() knex will return a number of affected rows
+  const count = await db('posts').where({ id }).del();
+  return count;
 }
--- api/posts/post-router.js
+++ api/posts/post-router.js
@@ -6,7 +6,7 @@ const router = express.Router()
 router.get('/', async (req, res, next) => {
   try {
     const data = await Post.get()
-    res.json(data)
+    res.status(200).json(data)
   } catch (err) {
     next(err)
   }
@@ -14,8 +14,7 @@ router.get('/', async (req, res, next) => {
 
 router.get('/:id', checkId, async (req, res, next) => {
   try {
-    const data = await Post.getById()
-    res.json(data)
+    res.json(req.post)
   } catch (err) {
     next(err)
   }
@@ -23,41 +22,69 @@ router.get('/:id', checkId, async (req, res, next) => {
 
 router.post('/', checkPayload, async (req, res, next) => {
   try {
-    const data = await Post.create()
+    const body = req.body
+    const data = await Post.create(body)
     res.json(data)
   } catch (err) {
     next(err)
   }
 })
 
-router.put('/:id', checkId, async (req, res,next ) => {
+router.put('/:id', checkId, checkPayload, async (req, res,next ) => 
+{
+  const { id } = req.params;
+  const changes = req.body;
   try {
-    const data = await Post.update()
-    res.json(data)
+    const data = await Post.update(id, changes);
+    res.json({ count: data });
   } catch (err) {
     next(err)
   }
 })
 
-router.delete('/:id', checkId, checkPayload, async (req, res, next) => {
+router.delete('/:id', checkId, async (req, res, next) => {
+  const { id } = req.params;
   try {
-    const data = await Post.remove()
-    res.json(data)
+    const data = await Post.remove(id)
+    res.json({ count: data })
   } catch (err) {
     next(err)
   }
 })
 
 router.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message, stack: err.stack })
+  err.statusCode = err.statusCode ? err.statusCode : 500;
+  res.status(err.statusCode).json({ message: err.message, stack: err.stack })
 })
 
-function checkId(req, res, next) {
-  next()
+async function checkId(req, res, next) {
+  const { id } = req.params;
+  try {
+    const post = await Post.getById(id);
+    if (post) {
+      req.post = post;
+      next()
+    } else {
+      const err = new Error('invalid id');
+      err.statusCode = 404;
+      next(err)
+    }
+  } catch (err) {
+    err.statusCode = 500;
+    err.message = 'error retrieving a post';
+    next(err)
+  }
 }
 
 function checkPayload(req, res, next) {
-  next()
+  const body = req.body
+  if (!body.title || !body.contents) {
+    const err = new Error('body must include "tilte" and "contents"');
+    err.statusCode = 400;
+    next(err);
+  } else {
+    next();
+  }
 }
 
 module.exports = router
